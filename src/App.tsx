7127b788ed882301793087e35abfd2d448f964ac
ohizz <jtsamuel3084@gmail.com>
2023-09-24 17:00:16 +0100

    comment both componenet causing the error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from './components/Header'
-import MainBody from './components/MainBody'
-import SideBar from './components/SideBar'
+// import MainBody from './components/MainBody'
+// import SideBar from './components/SideBar'
 
 
 function App() {
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className='relative max-w-[1440px] bg-[#F9FAFF]'>
       <div className='flex justify-between items-start mr-20'>
-      <SideBar/>
+      {/* <SideBar/> */}
      <Header/>
       </div>
-      <MainBody/>
+      {/* <MainBody/> */}
     </div>
   )
 }
