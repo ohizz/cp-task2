d615e7e9a632242bf37a7bf8717b873fe22fb61c
ohizz <jtsamuel3084@gmail.com>
2023-09-24 17:14:56 +0100

    please work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import './App.css'
 import Header from './components/Header'
-// import MainBody from './components/MainBody'
-// import SideBar from './components/SideBar'
-
+import MainBody from './components/MainBody'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <div className='relative max-w-[1440px] bg-[#F9FAFF]'>
       <div className='flex justify-between items-start mr-20'>
-      {/* <SideBar/> */}
+      <SideBar/>
      <Header/>
       </div>
-      {/* <MainBody/> */}
+      <MainBody/>
     </div>
   )
 }
